d2740384c59d8965aecf7cd72fd53cc6aacf2280
Birtalan Robert <robert.birtalan@fortech.ro>
2023-05-13 18:27:32 +0300

    reservation handling

---
+++ hooks/useReservation.ts
@@ -0,0 +1,66 @@
+import { Dispatch, SetStateAction, useState } from "react";
+import axios from "axios";
+import { Time } from "../utils/convertToDisplayTime";
+
+export default function useReservation() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const createReservation = async ({
+    slug,
+    partySize,
+    day,
+    time,
+    bookerFirstName,
+    bookerLastName,
+    bookerEmail,
+    bookerPhone,
+    bookerOccasion,
+    bookerRequest,
+    setDidBook,
+  }: {
+    slug: string;
+    partySize: number;
+    day: string;
+    time: string;
+    bookerFirstName: string;
+    bookerLastName: string;
+    bookerEmail: string;
+    bookerPhone: string;
+    bookerOccasion?: string;
+    bookerRequest?: string;
+    setDidBook: Dispatch<SetStateAction<boolean>>
+  }) => {
+    setLoading(true);
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/api/restaurant/${slug}/reserve`,
+        {
+          bookerFirstName,
+          bookerLastName,
+          bookerEmail,
+          bookerPhone,
+          bookerOccasion,
+          bookerRequest,
+        },
+        {
+          params: {
+            day,
+            time,
+            partySize,
+          },
+        }
+      );
+
+      setLoading(false);
+      setDidBook(true);
+
+      return response.data;
+    } catch (error: any) {
+      setLoading(false);
+      setError(error.response.data.errorMessage);
+    }
+  };
+
+  return { loading, error, createReservation };
+}
